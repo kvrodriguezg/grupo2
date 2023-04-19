a38706d8cbbadf15b8a557e44c5e6bd05f95c68a
kv.rodriguezgarcia@gmail.com <kv.rodriguezgarcia@gmail.com>
2023-04-18 20:18:32 -0400

    Nuevo repositorio

---
+++ Tareagrupal6/script/funciones.js
@@ -0,0 +1,32 @@
+function validarNombre(nombre){
+    return nombre.length > 0
+};
+
+function ValidarMail(mail){
+    return mail.length > 0;
+};
+
+function validarMensaje(mensaje){
+    return mensaje.length > 0;
+};
+
+function validarFormulario(event){
+    event.preventDefault();
+    const nombre =  document.querySelector("input[type='text']").value;
+    const mail=  document.querySelector("input[type='email']").value;
+    const mensaje =  document.querySelector("textarea").value;
+if (!validarNombre(nombre)) {
+    alert("Ingresa tu nombre");
+    return;
+}
+if (!ValidarMail(mail)) {
+    alert("Ingresa tu e-mail");
+    return;
+}
+if (!validarMensaje(mensaje)) {
+    alert("Ingresa tu mensaje");
+    return;
+}
+alert("Recibimos tu mensaje")
+}
+document.querySelector("button[type='submit']").addEventListener('click',validarFormulario);
