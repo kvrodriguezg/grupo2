1bcdb207754fe349c08123f89a79106ccf5398d0
JohannCandia <jomacalla@gmail.com>
2023-04-19 21:20:51 -0400

    Update funciones.js
    rcomentario de fabian

--- script/funciones.js
+++ script/funciones.js
@@ -11,39 +11,6 @@
         return mensaje.length > 0;
     };
 
-<<<<<<< Updated upstream
-    function validarFormulario(event){
-        event.preventDefault();
-        const nombre =  document.querySelector("input[type='text']").value;
-        const mail=  document.querySelector("input[type='email']").value;
-        const mensaje =  document.querySelector("textarea").value;
-        if (!validarNombre(nombre)) {
-            alert("Ingresa tu nombre");
-            return;
-        }
-        if (!ValidarMail(mail)) {
-            alert("Ingresa tu e-mail");
-            return;
-        }
-        if (!validarMensaje(mensaje)) {
-            alert("Ingresa tu mensaje");
-            return;
-        }
-        alert("Recibimos tu mensaje")
-    }
-
-//LISTENER EN EL BOTON ENVIAR
-    if(document.querySelector("button[type='submit']")){
-        document.querySelector("button[type='submit']").addEventListener('click',validarFormulario);
-    }
-
-//DATATABLE DE ESTADISTICAS
-    if(document.querySelector("table[id='tablaProductos']")){
-        $(document).ready( function () {
-            $('#tablaProductos').DataTable()
-        } );
-    }
-=======
 function validarFormulario(event){
     event.preventDefault();
     const nombre =  document.querySelector("input[type='text']").value;
@@ -65,4 +32,3 @@ alert("Recibimos tu mensaje")
 }
 document.querySelector("button[type='submit']").addEventListener('click',validarFormulario);
 /* Comentario Rama Fabian **/
->>>>>>> Stashed changes
